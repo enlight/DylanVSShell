505cc9bb7bd2306faa31892eed6548da24a29f9a
Vadim Macagon <vadim.macagon@gmail.com>
2013-07-13 15:38:37 +1000

    Initial commit.
    This is the basic solution and projects that VS2012 generated for a
    Visual Studio Isolated Shell project. I've only changed the splash
    screen image.

---
+++ DylanVSShell/DylanVSShell.AboutBoxPackage/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace DylanVSShell.AboutBoxPackage
+{
+	static class GuidList
+	{
+		public const string guidAboutBoxPackagePkgString = "d4a8d047-057f-408f-a711-db84d70b2cb5";
+		public const string guidAboutBoxPackageCmdSetString = "f314a7b1-23a2-4858-aa81-a01c3e92cad5";
+
+		public static readonly Guid guidAboutBoxPackageCmdSet = new Guid(guidAboutBoxPackageCmdSetString);
+	};
+}
